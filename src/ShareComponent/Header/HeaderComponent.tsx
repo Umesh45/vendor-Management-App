37de5daabdbd98cb844bcb60dd853fa60202b111
Umesh Chauda <v-uchauda@microsoft.com>
2021-01-20 10:07:44 +0530

    changes

--- src/ShareComponent/Header/HeaderComponent.tsx
+++ src/ShareComponent/Header/HeaderComponent.tsx
@@ -15,26 +15,20 @@ export class HeaderComponent extends React.Component {
     render() {
         return (
             <Router>
-
                 <nav className="navbar navbar-expand-lg navbar-light">
                     <a className="navbar-brand" href="/"><img src={image} /></a>
-
                     <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNavDropdown"
                         aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
-
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
                                 <a className="nav-link active" href="/">HOME</a>
                             </li>
-
                             <li className="nav-item">
                                 <a className="nav-link" href="/vendor">VENDOR</a>
                             </li>
-
                             <li className="nav-item">
                                 <a className="nav-link" href="/about">ABOUT</a>
                             </li>
@@ -43,22 +37,14 @@ export class HeaderComponent extends React.Component {
                             </li>
                         </ul>
                     </div>
-
                 </nav>
                 <ErrorHandler>
                     <Route path="/vendor" component={VendorComponent} />
-                    <ErrorHandler/>
-                   
-                <Route exact path="/" component={Home} />
-               
-                <Route exact path="/about" component={About} />
-            
+                </ErrorHandler >
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
                     <Route exact path="/conatct" component={Contact} />
-              
-                
-
             </Router>
-        )
-    }
-
+        );
+}
 }
