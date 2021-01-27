b54f37daa0458bba1f7a0c58f2e1f5ec20b6a000
Umesh Chauda <v-uchauda@microsoft.com>
2021-01-27 12:15:00 +0530

    changes styling

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './About.css'
+import aboutImage from '../image/AboutImage.jpg';
 
 export class About extends React.Component {
     createError=()=>{
         console.log("Create error is called");
-       
     }
 
     render() {
         return (<>
         <div>
                 {/*<p style={{ marginTop: "100px" }}>About Us</p>*/}
-                <blockquote className="blockquote-reverse" style={{ marginTop:"200px" }}>
+               {/* <img src={aboutImage} className='img1'/> */}
+                <blockquote className="blockquote-reverse" style={{ marginTop:"150px" }}>
+                    <h4 className='about'>About us: </h4>
+                    {/* <div className='bottom-line'></div> */}
                     <p className='about'><b>With the help of this application,you can add vendor,can view the list of available
                     vendor at the required location also can do edit their details if needs to be as
                         well as can also remove them ,if they are being deactivated or not providing services now.</b></p>
-                </blockquote><br/>
+                </blockquote>
+                {/* <div className='bottom-line'></div> */}
+
             </div>
         </>)
     }
